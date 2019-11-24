ec12634fd0c6d0acd1948e7a6e2c5f80b23f0cc2
Olena Matokhina <olena@matokhina.com>
2019-11-24 16:26:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,5 +20,8 @@ end
 def monopoly_with_fourth_tier
   hash_3 = monopoly_with_third_tier()
   hash_3[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+  hash_3[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+  hash_3[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+  hash_3[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
   return hash_3
 end
