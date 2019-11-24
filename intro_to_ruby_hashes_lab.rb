02303bc5842850b969613839315aec831fcccc30
Olena Matokhina <olena@matokhina.com>
2019-11-24 15:55:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ end
 
 def monopoly_with_third_tier
   hash_2 = monopoly_with_second_tier()
-  hash_2[:railroads][:rent_in_dollars] => 15
+  hash_2[:railroads][:rent_in_dollars] = 15
   return hash_2
 end
 
